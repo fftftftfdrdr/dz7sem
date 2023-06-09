e9c6fe9604d82e0156e343c19203d4dca0c6a8ca
Ivan Slukin <provaider010@gmail.com>
2023-06-09 21:12:50 +1000

    dfdf

---
+++ DZ_2/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает значение элемента в двумерном массиве,
+//  и возвращает позицию этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+Console.Clear();
+System.Console.WriteLine("Введите количество строк ");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов ");
+int cols = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int [rows,cols];
+FillArray(array);
+PrintArray(array);
+FindNum(array);
+
+void FillArray(int[,] array)
+{
+    for (int i =0;i<array.GetLength(0);i++)
+        for (int j =0;j<array.GetLength(1);j++)
+            array[i,j] = new Random().Next(11);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i =0;i<array.GetLength(0);i++)
+        {
+        for (int j =0;j<array.GetLength(1);j++)
+            Console.Write($"{array[i,j],3}    ");
+        System.Console.WriteLine();
+        }
+}
+
+
+void FindNum(int[,] array)
+{
+  if (rows > array.GetLength(0) || cols > array.GetLength(1))
+{
+    Console.WriteLine("такого элемента нет");
+}  
+    for (int i =0;i<array.GetLength(0);i++)
+        {
+        for (int j =0;j<array.GetLength(1);j++)
+        
+        if(array[i, j] == num)
+        Console.Write($"({i},{j})  ");
+        }           
+}
